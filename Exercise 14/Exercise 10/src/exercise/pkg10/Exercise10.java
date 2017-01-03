aead8f25ceeb2f87b81f660feeaf82be6ef92abe
NabajitSaha <n4b4jit@gmail.com>
2017-01-03 20:02:43 +0530

    PATTERN
    123345
    1234
    123
    12
    1

---
+++ Exercise 14/Exercise 10/src/exercise/pkg10/Exercise10.java
@@ -0,0 +1,14 @@
+package exercise.pkg10;
+public class Exercise10 {
+    public static void main(String[] args) {
+        int i =5; int a;
+        for(;i>=1;i--)
+        {
+        for(a=1;a<=i;a++){
+            System.out.print(a);
+        }
+            System.out.println();
+        }
+                                            }
+    
+}
